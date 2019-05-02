ab08e1ae64b96d74d8c50058e700d7fa0a4967e9
justin <radium.laboratories@gmail.com>
2019-05-02 16:24:11 +0800

    red+green: wrote simple cypress test to check that login page has a title

--- cypress/integration/login_spec.js
+++ cypress/integration/login_spec.js
@@ -1,6 +1,7 @@
 describe('Login Page', () => {
   it('Shows a title and login form', () => {
-    // cy.visit('localhost:3000/login');
-    // cy.title().should('include', 'Login');
+    cy.visit('localhost:3000/login');
+    cy.title().should('contain', 'Login');
+    cy.get('h1').should('contain', 'Login');
   });
 });
--- src/Server.js
+++ src/Server.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = 3000;
 
+app.use(express.static('static'));
+
 app.get('/', (req, res) => res.send('Hello World'));
+app.get('/login', (req, res) => res.sendFile(path.join(__dirname, '/../views/login.html')));
 
 module.exports = app.listen(port, () => console.log(`Server running on port ${port}`));
