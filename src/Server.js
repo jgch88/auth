628b0cf4abbce606941b5635cbe063f4919f1d0a
justin <radium.laboratories@gmail.com>
2019-05-05 20:48:50 +0800

    setup babel cli to convert Server.js to use ES6 modules and use the import keyword

--- src/Server.js
+++ src/Server.js
@@ -1,9 +1,21 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+import bodyParser from 'body-parser';
+
+import Authenticator from './Authenticator';
+import UserService from './services/UserService';
+import InMemoryRepository from './services/InMemoryRepository';
+import SessionService from './services/SessionService';
+
+const inMemoryRepository = new InMemoryRepository();
+const userService = new UserService(inMemoryRepository);
+const authenticator = new Authenticator(userService);
+const sessionService = new SessionService();
 
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.json());
 app.use(express.static('static'));
 
 app.get('/', (req, res) => res.send('Hello World'));
@@ -12,7 +24,7 @@ app.get('/login', (req, res) => res.sendFile(path.join(__dirname, '/../views/log
 app.post('/login', (req, res) => res.status(401).send());
 
 app.post('/register', (req, res) => {
-  res.send(200);
+  res.sendStatus(200);
 });
 
 module.exports = app.listen(port, () => console.log(`Server running on port ${port}`));
