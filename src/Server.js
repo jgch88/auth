a229728352312baebd247c2cb066a14e33fc68b3
justin <radium.laboratories@gmail.com>
2019-05-05 19:25:09 +0800

    red+green: send http 401 on POST /login

--- src/Server.js
+++ src/Server.js
@@ -8,5 +8,6 @@ app.use(express.static('static'));
 
 app.get('/', (req, res) => res.send('Hello World'));
 app.get('/login', (req, res) => res.sendFile(path.join(__dirname, '/../views/login.html')));
+app.post('/login', (req, res) => res.status(401).send());
 
 module.exports = app.listen(port, () => console.log(`Server running on port ${port}`));
--- src/Server.test.js
+++ src/Server.test.js
@@ -6,9 +6,21 @@ afterEach(() => {
 });
 
 describe('server', () => {
-  // https://www.valentinog.com/blog/testing-api-koa-jest/
   test('exists and responds as expected', async () => {
     const response = await request(server).get('/');
     expect(response.status).toEqual(200);
+  });
+
+  describe('/login', () => {
+    it('returns HTTP status 401 when login is invalid', async () => {
+      const response = await request(server)
+        .post('/login')
+        .send({
+          username: 'user1',
+          password: 'password1',
+        });
+
+      expect(response.status).toEqual(401);
+    });
   });
 });
