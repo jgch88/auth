b8ac7e727c3b9f04d6fe70ee70abc5207f5d538f
justin <radium.laboratories@gmail.com>
2019-05-07 16:36:33 +0800

    red+green: (frontend) updated /login form test and view

--- cypress/integration/login_spec.js
+++ cypress/integration/login_spec.js
@@ -1,7 +1,9 @@
 describe('Login Page', () => {
-  it('Shows a title and login form', () => {
+  it('User can enter username and password', () => {
     cy.visit('localhost:3000/login');
     cy.title().should('contain', 'Login');
     cy.get('h1').should('contain', 'Login');
+    cy.get('#loginUsernameInput').type('user1');
+    cy.get('#loginPasswordInput').type('password1');
   });
 });
